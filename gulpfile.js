2926c75f8f73a8f33a03deda092e0cc9c908b128
test1 <test@gmail.com>
2020-05-29 17:08:24 +0900

    Footer layout

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,12 @@ var { series, parallel, watch, src, dest } = require('gulp')
 var sass = require('gulp-sass')
 var connect = require('gulp-connect')
 
-function clean(cb) {
+function clean() {
   console.log('clean')
 }
 
-function scss(cb) {
-  console.log('scss')
-  return src('./src/scss/*.scss')
+function scss() {
+  return src('./src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('./dist/css'))
     .pipe(connect.reload())
@@ -18,8 +17,15 @@ function html() {
   return src('./src/*.html').pipe(dest('./dist/')).pipe(connect.reload())
 }
 
+function img() {
+  return src('./src/img/**/*').pipe(desc('./dist/img/')).pipe(connect.reload())
+}
+
+function js() {
+  return src('./src/js/**/*.js').pipe(desc('./dist/js/')).pipe(connect.reload())
+}
+
 function connectDev() {
-  console.log('connectDev...')
   connect.server({
     root: './dist',
     port: 8000,
@@ -28,9 +34,10 @@ function connectDev() {
 }
 
 function watchDist() {
-  console.log('watchDist...')
-  watch('src/scss/*.scss', scss)
+  watch('src/scss/**/*.scss', scss)
   watch('src/*.html', html)
+  watch('src/img/**/*', img)
+  watch('src/js/**/*.js', js)
 }
 
 exports.watch = watchDist
