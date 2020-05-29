69cc297b52257ceda2a8b1607d5bc4ce8efbce6b
test1 <test@gmail.com>
2020-05-29 13:25:01 +0900

    add gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var { series, parallel, watch, src, dest } = require('gulp')
 var sass = require('gulp-sass')
+var connect = require('gulp-connect')
 
 function clean(cb) {
   console.log('clean')
@@ -9,11 +10,31 @@ function scss(cb) {
   console.log('scss')
   return src('./src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./src/css'))
+    .pipe(dest('./dist/css'))
+    .pipe(connect.reload())
+}
+
+function html() {
+  return src('./src/*.html').pipe(dest('./dist/')).pipe(connect.reload())
+}
+
+function connectDev() {
+  connect.server({
+    name: 'dev',
+    root: 'dist',
+    port: 8000,
+    livereload: true,
+  })
+}
+
+function watchDist() {
+  watch('src/scss/*.scss', scss)
+  watch('src/*.html', html)
 }
 
 exports.watch = function () {
-  watch('src/scss/*.scss', scss)
+  watchDist()
 }
+exports.dev = series(connectDev, watchDist)
 
 exports.build = series(clean, parallel(scss))
